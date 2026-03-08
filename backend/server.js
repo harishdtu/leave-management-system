ad064446fedc5d1b917e1d6c792f3479e2d9f38d
harishdtu <satyaharish12345@gmail.com>
2026-03-08 11:14:25 +0530

    add backend URL

--- backend/server.js
+++ backend/server.js
@@ -20,4 +20,8 @@ mongoose.connect(process.env.MONGO_URI)
 
 app.listen(process.env.PORT,()=>{
 console.log("Server running on port 5000");
+
+app.get("/", (req, res) => {
+  res.send("Leave Management API is running 🚀");
+});
 });
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://leave-management-system-81er.onrender.com/"
 });
 
 API.interceptors.request.use((req)=>{
